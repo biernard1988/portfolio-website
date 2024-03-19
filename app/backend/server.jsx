85f7613f3df5abe593d2affb575b6d4ff7058b9f
Bernard Almeida da Costa <biernard@gmail.com>
2024-03-19 19:19:30 +0100

    added cors and fixed dotenv

--- app/backend/server.jsx
+++ app/backend/server.jsx
@@ -2,12 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const projectRoutes = require("./routes/projects");
 
 //express app
 const app = express();
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -19,14 +21,11 @@ app.use("/api/projects", projectRoutes);
 
 //connect to db
 mongoose
-  .connect(process.env.NEXT_PUBLIC_MONGO_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => {
     //listen for requests
-    app.listen(process.env.NEXT_PUBLIC_PORT, () => {
-      console.log(
-        "connected to db & listening on port",
-        process.env.NEXT_PUBLIC_PORT
-      );
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
     });
   })
   .catch((error) => {
