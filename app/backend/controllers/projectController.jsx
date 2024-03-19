a44dfa76f670535315d5845d93f59dd23e83094a
Bernard Almeida da Costa <biernard@gmail.com>
2024-03-19 19:01:38 +0100

    removed the second .env file, updated the .env.local and methods in projectController

--- app/backend/controllers/projectController.jsx
+++ app/backend/controllers/projectController.jsx
@@ -1,82 +1,74 @@
-const Project = require('../models/projectModel');
-const mongoose = require('mongoose');
+const Project = require("../models/projectModel");
 
 // Get all projects
 const getProjects = async (req, res) => {
-    try {
-        const projects = await Project.find({});
-        res.status(200).json(projects);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const projects = await Project.find({});
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 // Get a single project
 const getProject = async (req, res) => {
-    const { id } = req.params;
-    try {
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(404).json({ error: "No such project" });
-        }
-        const project = await Project.findById(id);
-        if (!project) {
-            return res.status(404).json({ error: "No such project" });
-        }
-        res.status(200).json(project);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  const { id } = req.params;
+  try {
+    const project = await Project.findById(id);
+    res.status(200).json(project);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 // Create new project
 const createProject = async (req, res) => {
-    const { title, description, image, tags, gitUrl, previewUrl } = req.body;
-    try {
-        const project = await Project.create({ title, description, image, tags, gitUrl, previewUrl });
-        res.status(201).json(project);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+  const { title, description, image, tags, gitUrl, previewUrl } = req.body;
+  try {
+    const project = await Project.create({
+      title,
+      description,
+      image,
+      tags,
+      gitUrl,
+      previewUrl,
+    });
+    res.status(201).json(project);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // Delete a project
 const deleteProject = async (req, res) => {
-    const { id } = req.params;
-    try {
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(404).json({ error: "No such project" });
-        }
-        const project = await Project.findOneAndDelete({ _id: id });
-        if (!project) {
-            return res.status(404).json({ error: "No such project" });
-        }
-        res.status(200).json(project);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  const { id } = req.params;
+  try {
+    const project = await Project.findByIdAndDelete({ _id: id });
+    res.status(200).json(project);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 // Update a project
 const updateProject = async (req, res) => {
-    const { id } = req.params;
-    try {
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(404).json({ error: "No such project" });
-        }
-        const project = await Project.findOneAndUpdate({ _id: id }, { ...req.body }, { new: true });
-        if (!project) {
-            return res.status(404).json({ error: "No such project" });
-        }
-        res.status(200).json(project);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  const { id } = req.params;
+  try {
+    const project = await Project.findByIdAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { new: true }
+    );
+    res.status(200).json(project);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = {
-    getProjects,
-    getProject,
-    createProject,
-    deleteProject,
-    updateProject
+  getProjects,
+  getProject,
+  createProject,
+  deleteProject,
+  updateProject,
 };
--- app/backend/server.jsx
+++ app/backend/server.jsx
@@ -1,32 +1,34 @@
-require ('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const projectRoutes = require('./routes/projects')
-
+const express = require("express");
+const mongoose = require("mongoose");
+const projectRoutes = require("./routes/projects");
 
 //express app
-const app = express()
+const app = express();
 
 //middleware
-app.use(express.json())
-app.use((req,res,next)=>{
-    console.log(req.path, req.method)
-    next()
-})
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 //routes
-app.use('/api/projects',projectRoutes)
+app.use("/api/projects", projectRoutes);
 
 //connect to db
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
+mongoose
+  .connect(process.env.NEXT_PUBLIC_MONGO_URI)
+  .then(() => {
     //listen for requests
-app.listen(process.env.PORT,()=>{
-    console.log('connected to db & listening on port', process.env.PORT)
-})
-})
-.catch((error)=>{
-    console.log(error)
-})
-
+    app.listen(process.env.NEXT_PUBLIC_PORT, () => {
+      console.log(
+        "connected to db & listening on port",
+        process.env.NEXT_PUBLIC_PORT
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
